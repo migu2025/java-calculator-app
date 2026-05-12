03c0571ef8b809899c08f88164d432ecfadb1f3b
Miguel <migper88@hotmail.com>
2026-05-12 11:53:10 +0200

    metodo con return

--- src/main/java/com/javacourse/basics/Main.java
+++ src/main/java/com/javacourse/basics/Main.java
@@ -4,12 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++) {
+        int risultato = somma(5, 3);
+        System.out.println(risultato);
 
-            System.out.println("5 * " + i + " = " + (5 * i));
+        int moltiplicaz = moltiplica(2, 25);
+        System.out.println("Il risultato è:" + moltiplicaz);
 
-        }
+    }
 
+    public static int somma(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public static int moltiplica(int a, int b) {
+        return a * b;
     }
 
 }
