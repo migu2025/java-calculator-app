9c4bb2e24ff1a836491fe423ed36df0d4f5748a8
Miguel <migper88@hotmail.com>
2026-05-12 22:41:19 +0200

    feat: add arrays and foreach examples

--- src/main/java/com/javacourse/basics/Main.java
+++ src/main/java/com/javacourse/basics/Main.java
@@ -3,21 +3,19 @@ package com.javacourse.basics;
 public class Main {
 
     public static void main(String[] args) {
+        int[] numeri = {10, 20, 30, 40, 50};
+        String[] nome = {"Jacy", "Sabri", "Ver", "Alice", "Linda"};
 
-        int risultato = somma(5, 3);
-        System.out.println(risultato);
+        for (int i : numeri) {
+            System.out.print(i + " ");
 
-        int moltiplicaz = moltiplica(2, 25);
-        System.out.println("Il risultato è:" + moltiplicaz);
+        }
+        for (String n : nome) {
+            System.out.println(n);
+        }
+        System.out.println(numeri[0]);
+        System.out.println(numeri[4]);
 
-    }
-
-    public static int somma(int numero1, int numero2) {
-        return numero1 + numero2;
-    }
-
-    public static int moltiplica(int a, int b) {
-        return a * b;
     }
 
 }
